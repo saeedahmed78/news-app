5a6e68271b2111c88853203e64a669409e07ee18
saeedahmed78 <38943150+saeedahmed78@users.noreply.github.com>
2018-08-25 19:02:08 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-        function check(){
-        $.ajax({
-            url: "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=2d0fcf8a083a44d5941c9cd307303ddb",
-            success:function(data){
-                console.log(data);
-                for(i=0 ; i<data.articles.length;i++){
-                    console.log("lanat")
-                    document.querySelector(".gen1").innerHTML += `
-                        <div class="bg-img" style="background-image: url(${data.articles[i].urlToImage})">
-                    <div class="bg-dark">
-                    
-                    <div class="title n1">${data.articles[i].title}</div>
-        
-                </div>       
-                </div>`;
-                    
-            }
+					function check(){
+					$.ajax({
+						url: "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=2d0fcf8a083a44d5941c9cd307303ddb",
+						success:function(data){
+							console.log(data);
+							for(i=0 ; i<data.articles.length;i++){
+								console.log("lanat")
+								document.querySelector(".section1").innerHTML += `
+						<div class="bg-img" style="background-image: url(${data.articles[i].urlToImage})">
+						<div class="bg-dark">		
+						<a href = "${data.articles[i].url}" style = "color:white;">                                           
+						<div class="title n1">${data.articles[i].title}</div>
+						</a>
+						</div>       
+						</div>`;
+								
+						}
 
-        }
+					}
 
-        })
+					})
 
-            $.ajax({
-                url: "https://newsapi.org/v2/everything?sources=bbc-news&apiKey=2d0fcf8a083a44d5941c9cd307303ddb",
-            success:function(data){
-                console.log(data);
-                        for(let i = 0;  i<=19; i++){
-                document.querySelector(".gen2").innerHTML +=`
-                <div class="bg-img" style="background-image: url(${data.articles[i].urlToImage})">
-                <div class="bg-dark">
-            
-                <div class="title n1">${data.articles[i].title}</div>
-
-            </div>       
-            </div>`;
-            }
-                }   
-        })
-        }
+						$.ajax({
+						url: "https://newsapi.org/v2/everything?sources=bbc-news&apiKey=2d0fcf8a083a44d5941c9cd307303ddb",
+						success:function(data){
+						console.log(data);
+						for(let i = 0;  i<=19; i++){
+						document.querySelector(".section2").innerHTML +=`
+						<div class="bg-img" style="background-image: url(${data.articles[i].urlToImage})">
+						<div class="bg-dark">
+						<a href = "${data.articles[i].url}" style = "color:white;">                   
+						<div class="title n1">${data.articles[i].title}</div>
+						</a>                
+						</div>       
+						</div>`;
+						}
+							}   
+					})
+					}
