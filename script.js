33ee1be7c47f13bc8196b5b3f41560f947544212
saeedahmed78 <38943150+saeedahmed78@users.noreply.github.com>
2018-08-25 18:21:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+        function check(){
+        $.ajax({
+            url: "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=2d0fcf8a083a44d5941c9cd307303ddb",
+            success:function(data){
+                console.log(data);
+                for(i=0 ; i<data.articles.length;i++){
+                    console.log("lanat")
+                    document.querySelector(".gen1").innerHTML += `
+                        <div class="bg-img" style="background-image: url(${data.articles[i].urlToImage})">
+                    <div class="bg-dark">
+                    
+                    <div class="title n1">${data.articles[i].title}</div>
+        
+                </div>       
+                </div>`;
+                    
+            }
+
+        }
+
+        })
+
+            $.ajax({
+                url: "https://newsapi.org/v2/everything?sources=bbc-news&apiKey=2d0fcf8a083a44d5941c9cd307303ddb",
+            success:function(data){
+                console.log(data);
+                        for(let i = 0;  i<=19; i++){
+                document.querySelector(".gen2").innerHTML +=`
+                <div class="bg-img" style="background-image: url(${data.articles[i].urlToImage})">
+                <div class="bg-dark">
+            
+                <div class="title n1">${data.articles[i].title}</div>
+
+            </div>       
+            </div>`;
+            }
+                }   
+        })
+        }
